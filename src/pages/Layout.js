be4bcad6eaa308e25ea1fb375d4e91ab16ddc77e
blake <bdubb27@gmail.com>
2022-07-07 04:35:32 -0700

    added layout route for AAC

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -13,6 +13,9 @@ export default function Layout() {
             <Link to="/observations">Observations</Link>
           </li>
           <li>
+            <Link to="/observations/aac">AAC</Link>
+          </li>
+          <li>
             <Link to="/observations/get">Get Latest File</Link>
           </li>
           <li>
